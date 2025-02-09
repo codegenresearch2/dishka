59a5bc43d1c84c8a8ee2e69de5953ccfe06de9ce
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 19:44:11 +0000

    Reset to 8e571da2b481df56c2b1bc40cfcd9c179346c3fc and made changes for LLM Round 1

--- src/dishka/registry.py
+++ src/dishka/registry.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Any, List, NewType, Type
+from typing import Any, List, Type
 
 from .dependency_source import Factory
 from .provider import Provider
@@ -23,8 +23,8 @@ class Registry:
 def make_registries(
         *providers: Provider, scopes: Type[BaseScope],
 ) -> List[Registry]:
-    dep_scopes = {}
-    alias_sources = {}
+    dep_scopes: dict[Type, BaseScope] = {}
+    alias_sources: dict[Type, Type] = {}
     for provider in providers:
         for source in provider.factories:
             dep_scopes[source.provides] = source.scope
