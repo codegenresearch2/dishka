4a4ab0cb2c6db97800c05dd97ecb8d216f13abbd
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 12:42:05 +0000

    Reset to 574d1bebd2bf20bc032dca0b8b61e38ce516bd9e and made changes for LLM Round 1

--- src/dishka/registry.py
+++ src/dishka/registry.py
@@ -10,7 +10,7 @@ class Registry:
     __slots__ = ('scope', '_factories')
 
     def __init__(self, scope: BaseScope):
-        self._factories = {}
+        self._factories: dict[Type, Factory] = {}
         self.scope = scope
 
     def add_provider(self, factory: Factory):
@@ -32,7 +32,7 @@ def make_registries(
 
     registries = {scope: Registry(scope) for scope in scopes}
 
-    decorator_depth = defaultdict(int)
+    decorator_depth: dict[Type, int] = defaultdict(int)
 
     for provider in providers:
         for source in provider.dependency_sources:
