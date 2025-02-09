e59d5623071a4acf6003042920ffed6c8d529c7c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 20:02:23 +0000

    Reset to 8e571da2b481df56c2b1bc40cfcd9c179346c3fc and made changes for LLM Round 1

--- src/dishka/registry.py
+++ src/dishka/registry.py
@@ -23,8 +23,8 @@ class Registry:
 def make_registries(
         *providers: Provider, scopes: Type[BaseScope],
 ) -> List[Registry]:
-    dep_scopes = {}
-    alias_sources = {}
+    dep_scopes: dict[Type, BaseScope] = {}
+    alias_sources: dict[Type, Type] = {}
     for provider in providers:
         for source in provider.factories:
             dep_scopes[source.provides] = source.scope
