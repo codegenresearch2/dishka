b06db7a490f752ef1154becfd8fcd43aa2c66bc9
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 12:07:03 +0000

    Reset to dd8e0c6958106ea2509ed5374119968c51f13a3d and made changes for LLM Round 1

--- src/dishka/integrations/fastapi.py
+++ src/dishka/integrations/fastapi.py
@@ -13,16 +13,16 @@ from .base import Depends, wrap_injection
 
 def inject(func):
     hints = get_type_hints(func)
-    requests_param = next(
+    request_param = next(
         (name for name, hint in hints.items() if hint is Request),
         None,
     )
-    if requests_param:
+    if request_param:
         additional_params = []
     else:
-        requests_param = '____@request'
+        request_param = '____dishka_request'
         additional_params = [Parameter(
-            name=requests_param,
+            name=request_param,
             annotation=Request,
             kind=Parameter.KEYWORD_ONLY,
         )]
@@ -30,7 +30,7 @@ def inject(func):
     return wrap_injection(
         func=func,
         remove_depends=True,
-        container_getter=lambda kw: kw[requests_param].state.dishka_container,
+        container_getter=lambda kw, p: kw[p],
         additional_params=additional_params,
         is_async=True,
     )
