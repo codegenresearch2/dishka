25dfb9db33792d526f2bc2ca51cd737e7d3c555a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 07:51:30 +0000

    Reset to dd8e0c6958106ea2509ed5374119968c51f13a3d and made changes for LLM Round 1

--- src/dishka/integrations/fastapi.py
+++ src/dishka/integrations/fastapi.py
@@ -1,9 +1,5 @@
-__all__ = [
-    'Depends', 'inject', 'DishkaApp',
-]
-
 from inspect import Parameter
-from typing import Sequence, get_type_hints
+from typing import Sequence
 
 from fastapi import FastAPI, Request
 
@@ -21,27 +17,29 @@ def inject(func):
         additional_params = []
     else:
         request_param = '____dishka_request'
-        additional_params = [Parameter(
-            name=request_param,
-            annotation=Request,
-            kind=Parameter.KEYWORD_ONLY,
-        )]
+        additional_params = [
+            Parameter(
+                name=request_param,
+                annotation=Request,
+                kind=Parameter.KEYWORD_ONLY,
+            )
+        ]
 
     return wrap_injection(
         func=func,
         remove_depends=True,
-        container_getter=lambda _, p: p['____dishka_request'].state.dishka_container,
+        container_getter=lambda _, p: p[request_param].state.dishka_container,
         additional_params=additional_params,
         is_async=True,
     )
 
 
-async def add_request_container_middleware(request_dishka_request: Request, call_next):
-    async with request_dishka_request.app.state.dishka_container(
-            {Request: request_dishka_request},
+async def add_request_container_middleware(request: Request, call_next):
+    async with request.app.state.dishka_container(
+            {Request: request},
     ) as request_container:
-        request_dishka_request.state.dishka_container = request_container
-        return await call_next(request_dishka_request)
+        request.state.dishka_container = request_container
+        return await call_next(request)
 
 
 class DishkaApp:
