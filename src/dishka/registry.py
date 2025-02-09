3ecd5eaa40347de28f01b909e70c246df440bca4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 12:18:59 +0000

    Reset to 8e571da2b481df56c2b1bc40cfcd9c179346c3fc and made changes for LLM Round 1

--- src/dishka/registry.py
+++ src/dishka/registry.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Any, List, NewType, Type
 
 from .dependency_source import Factory
@@ -44,7 +45,7 @@ def make_registries(
             while alias_source not in dep_scopes:
                 alias_source = alias_sources[alias_source]
                 if alias_source in visited_types:
-                    raise ValueError(f'Cycle aliases detected {visited_types}')
+                    raise ValueError(f"Cycle aliases detected {visited_types}")
                 visited_types.append(alias_source)
             scope = dep_scopes[alias_source]
             dep_scopes[source.provides] = scope
