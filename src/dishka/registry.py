2af26755b441d038c98159a2e1cba35d50422bc8
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 20:02:23 +0000

    Reset to 574d1bebd2bf20bc032dca0b8b61e38ce516bd9e and made changes for LLM Round 1

--- src/dishka/registry.py
+++ src/dishka/registry.py
@@ -1,5 +1,4 @@
-from typing import Any, List, Type, Dict, DefaultDict
-from collections import defaultdict
+from typing import Any, List, Type, Dict
 
 from .dependency_source import Alias, Decorator, Factory
 from .provider import Provider
@@ -16,18 +15,18 @@ class Registry:
     def add_provider(self, provider: Factory):
         self._factories[provider.provides] = provider
 
-    def get_provider(self, dependency: Any):
+    def get_provider(self, dependency: Any) -> Factory:
         return self._factories.get(dependency)
 
 
 def make_registries(
         *providers: Provider, scopes: Type[BaseScope]
 ) -> List[Registry]:
-    dependency_scopes: Dict[Type, BaseScope] = {}
+    dep_scopes: Dict[Type, BaseScope] = {}
     for provider in providers:
         for source in provider.dependency_sources:
             if hasattr(source, "scope"):
-                dependency_scopes[source.provides] = source.scope
+                dep_scopes[source.provides] = source.scope
 
     registries: Dict[BaseScope, Registry] = {scope: Registry(scope) for scope in scopes}
 
@@ -36,17 +35,17 @@ def make_registries(
             if isinstance(source, Factory):
                 scope = source.scope
             elif isinstance(source, Alias):
-                scope = dependency_scopes[source.source]
-                dependency_scopes[source.provides] = scope
-                source = source.as_provider(scope)
+                scope = dep_scopes[source.source]
+                dep_scopes[source.provides] = scope
+                source = source.as_factory(scope)
             elif isinstance(source, Decorator):
-                scope = dependency_scopes[source.provides]
+                scope = dep_scopes[source.provides]
                 registry = registries[scope]
                 undecorated_type = type(f"Old_{source.provides.__name__}", (source.provides,), {})
                 old_provider = registry.get_provider(source.provides)
                 old_provider.provides = undecorated_type
                 registry.add_provider(old_provider)
-                source = source.as_provider(scope, undecorated_type)
+                source = source.as_factory(scope, undecorated_type)
             else:
                 raise ValueError("Unknown dependency source type")
             registries[scope].add_provider(source)
