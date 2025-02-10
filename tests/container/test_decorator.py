8c13e884662d12e9bd086aafc4b3c5e212f387c6
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 13:01:07 +0000

    Reset to 574d1bebd2bf20bc032dca0b8b61e38ce516bd9e and made changes for LLM Round 1

--- tests/container/test_decorator.py
+++ tests/container/test_decorator.py
@@ -1,42 +1,36 @@
 from dishka import Provider, Scope, alias, decorate, make_container, provide
 
-
 class A:
     pass
-
 
 class A1(A):
     pass
 
-
 class A2(A1):
     pass
-
 
 class ADecorator:
     def __init__(self, a: A):
         self.a = a
 
-
 def test_simple():
     class MyProvider(Provider):
-        a = provide(A, scope=Scope.APP)
-        ad = decorate(ADecorator, provides=A)
+        a_instance = provide(A, scope=Scope.APP)
+        a_decorated = decorate(ADecorator, provides=A)
 
     with make_container(MyProvider()) as container:
         a = container.get(A)
         assert isinstance(a, ADecorator)
         assert isinstance(a.a, A)
 
-
 def test_alias():
     class MyProvider(Provider):
-        a2 = provide(A2, scope=Scope.APP)
-        a1 = alias(source=A2, provides=A1)
-        a = alias(source=A1, provides=A)
+        a2_instance = provide(A2, scope=Scope.APP)
+        a1_alias = alias(source=A2, provides=A1)
+        a_alias = alias(source=A1, provides=A)
 
         @decorate
-        def decorated(self, a: A1) -> A1:
+        def decorate_a(self, a: A1) -> A1:
             return ADecorator(a)
 
     with make_container(MyProvider()) as container:
@@ -50,15 +44,3 @@ def test_alias():
 
         a = container.get(A)
         assert a is a1
-
-def test_double():
-    class MyProvider(Provider):
-        a = provide(A, scope=Scope.APP)
-        ad = decorate(ADecorator, provides=A)
-        ad2 = decorate(ADecorator, provides=A)
-
-    with make_container(MyProvider()) as container:
-        a = container.get(A)
-        assert isinstance(a, ADecorator)
-        assert isinstance(a.a, ADecorator)
-        assert isinstance(a.a.a, A)
