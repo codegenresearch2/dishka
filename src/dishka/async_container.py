1abde3b6ab7ab7451948d1fc2a8afc1c2a298ddd
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 15:50:20 +0000

    Reset to 8e571da2b481df56c2b1bc40cfcd9c179346c3fc and made changes for LLM Round 1

--- src/dishka/async_container.py
+++ src/dishka/async_container.py
@@ -121,11 +121,9 @@ class AsyncContainer:
                     await anext(exit_generator.callable)
                 elif exit_generator.type is FactoryType.GENERATOR:
                     next(exit_generator.callable)
-            except StopIteration:
+            except (StopIteration, StopAsyncIteration):
                 pass
-            except StopAsyncIteration:
-                pass
-            except Exception as err:
+            except Exception as err:  # noqa: BLE001
                 e = err
         if e:
             raise e
