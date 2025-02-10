5928258baf803198a833e0a44fe3a2c24cc179cb
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 14:28:28 +0000

    Reset to dd8e0c6958106ea2509ed5374119968c51f13a3d and made changes for LLM Round 1

--- src/dishka/integrations/fastapi.py
+++ src/dishka/integrations/fastapi.py
@@ -12,16 +12,16 @@ from .base import Depends, wrap_injection
 
 def inject(func):
     hints = get_type_hints(func)
-    request_param = next(
+    dishka_request_param = next(
         (name for name, hint in hints.items() if hint is Request),
         None,
     )
-    if request_param:
+    if dishka_request_param:
         additional_params = []
     else:
-        request_param = '____@request'
+        dishka_request_param = '____dishka_request'
         additional_params = [Parameter(
-            name=request_param,
+            name=dishka_request_param,
             annotation=Request,
             kind=Parameter.KEYWORD_ONLY,
         )]
@@ -29,12 +29,12 @@ def inject(func):
     return wrap_injection(
         func=func,
         remove_depends=True,
-        container_getter=lambda kw: kw[request_param].state.dishka_container,
+        container_getter=lambda _, p: p[dishka_request_param].state.dishka_container,
         additional_params=additional_params,
         is_async=True,
     )
 
-async def request_container_middleware(request: Request, call_next):
+async def add_request_container_middleware(request: Request, call_next):
     async with request.app.state.dishka_container({Request: request}) as request_container:
         request.state.dishka_container = request_container
         return await call_next(request)
@@ -43,7 +43,7 @@ class DishkaApp:
     def __init__(self, providers: Sequence[Provider], app: FastAPI):
         self.app = app
         self.container_wrapper = make_async_container(*providers)
-        self.app.middleware('http')(request_container_middleware)
+        self.app.middleware('http')(add_request_container_middleware)
 
     async def __call__(self, scope, receive, send):
         if scope['type'] == 'lifespan':
@@ -54,6 +54,6 @@ class DishkaApp:
                 elif message['type'] == 'lifespan.shutdown':
                     await self.app.state.dishka_container.__aexit__(None, None, None)
 
-            await self.app(scope, my_recv, send)
+            return await self.app(scope, my_recv, send)
         else:
             return await self.app(scope, receive, send)
